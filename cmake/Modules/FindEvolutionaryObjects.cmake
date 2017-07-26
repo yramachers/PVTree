92f623d50cf861347905521eb5823897773c4a5a
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-07-26 12:37:43 +0100

    Create FindXXX for EvolutionaryObjects
    As EvolutionaryObjects are not fully used yet, move code to locate
    headers/libraries to FinxXXX CMake module for later use.

---
+++ cmake/Modules/FindEvolutionaryObjects.cmake
@@ -0,0 +1,10 @@
+# Evolutionary Objects (Not yet used)
+#find_package(EvolutionaryObjects)
+#set(EvolutionaryObjects_LIBRARY_DIR ${EvolutionaryObjects_DIR}/lib)
+#set(EvolutionaryObjects_INCLUDE_DIR ${EvolutionaryObjects_DIR}/include/eo)
+#set(EvolutionaryObjects_LIBS "-L${EvolutionaryObjects_LIBRARY_DIR} -lcma  -leo  -leoserial  -leoutils  -les  -lga")
+#Also include evolutionary computing library
+#include_directories(BEFORE ${EvolutionaryObjects_INCLUDE_DIR})
+#set(LIBS ${LIBS} ${EvolutionaryObjects_LIBS})
+
+
